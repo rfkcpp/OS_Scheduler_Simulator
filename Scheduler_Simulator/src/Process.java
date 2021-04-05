adc658cb8d42fccfa61116c8f1bceaa303d64c3b
rfkcpp <rkeshishian@cpp.edu>
2021-04-04 21:02:05 -0700

    Process class created

---
+++ Scheduler_Simulator/src/Process.java
@@ -0,0 +1,84 @@
+
+public class Process {
+	
+	private int id;
+	private boolean active;
+	private int arrival_time;
+	private int total_cpu_time;
+	private int remaining_cpu_time;
+	private int turnaround_time;
+	
+	
+	
+	//constructor
+	public Process(int my_id,int total_time) {
+		
+		id = my_id;
+		total_cpu_time = total_time;
+		
+	}//end of constructor
+	
+	
+	
+	
+	//setter methods ---------------------------------------------------------------------------
+	
+	public void set_active(boolean state) {
+		active = state;
+	}
+	
+	public void set_arrival_time(int time) {
+		arrival_time = time;
+	}
+	
+	public void set_remaining_cpu_time(int time) {
+		remaining_cpu_time = time;
+	}
+	
+	public void set_turnaround_time(int time) {
+		turnaround_time = time;
+	}
+	
+	//end of setter methods------------------------------------------------------------------------
+	
+	
+	
+	//getter methods ------------------------------------------------------------------------------
+	
+	public int get_id() {
+		return id;
+	}
+	
+	public boolean is_active() {
+		return active;
+	}
+	
+	public int get_arrival_time() {
+		return arrival_time;
+	}
+	
+	public int get_total_cpu_time() {
+		return total_cpu_time;
+	}
+	
+	public int get_remianing_cpu_time() {
+		return remaining_cpu_time;
+	}
+	
+	public int get_turnaround_time() {
+		return turnaround_time;
+	}
+	
+	//end of getter methods --------------------------------------------------------------------------
+	
+	
+	//this function returns true if the process has been executed and finished
+	public boolean is_done() {
+		
+		return remaining_cpu_time == 0;
+	
+	}//end of is_done
+
+
+
+}//end of Process class
